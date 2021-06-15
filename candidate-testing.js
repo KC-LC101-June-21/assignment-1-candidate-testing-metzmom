87e5f6bb617ee6089455fcb463d5e93e4c3c068f
Margie Metz <84488543+metzmom@users.noreply.github.com>
2021-06-15 18:37:30 +0000

    cleaned up comments

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,6 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
-// let score = 0;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -20,51 +19,8 @@ function askForName() {
   //console.log("Hello, Commrade " + candidateName + "!");
 }
 
-
-/*
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  //candidateAnswer = input.question(question);
-  //
-  
-
-  for (let i = 0; i < questions.length; i++) {
-    candidateAnswers[i] = (input.question("\n" + (i+1) + ") " + questions[i]+" "));
-    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
-      score = score + 1;
-    }    
-    else {
-      //console.log(`You are wrong! Your answer ${candidateAnswers} is not correct.`) 
-    }
-     console.log(`Your Answer: ${candidateAnswers[i]}`);
-     console.log(`Correct Answer: ${correctAnswers[i]}`);
-     console.log();
-  }   
-}
-
-
-function gradeQuiz(candidateAnswers) {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    let grade;
-    grade = (score/questions.length)*100; 
-    if (grade >= 80) {
-      status = "PASSED";
-    }
-    else {
-      status = "FAILED";
-    } 
-    console.log(`>>> OVERALL GRADE: ${grade}% (${score} of ${(questions.length)} responses correct)   <<<\n>>> STATUS:  ${status} <<<`);
-
-//console.log("grade " + grade);
-//console.log("typeof " + typeof grade);
-
-    return grade;
-}
-*/
-
-
-
-function askQuestion() {
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers[i] = (input.question("\n" + (i+1) + ") " + questions[i]+" "));  
 
@@ -76,8 +32,7 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  //determine if answers to individual questions are right or wrong and keep 
-  //track of score
+  //determine if answers to individual questions are right or wrong and keep track of score 
   let score = 0;
   for (let i = 0; i < questions.length; i++) {
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
@@ -94,7 +49,6 @@ function gradeQuiz(candidateAnswers) {
     status = "FAILED";
   } 
   console.log(`>>> OVERALL GRADE: ${grade}% (${score} of ${(questions.length)} responses correct)   <<<\n>>> STATUS:  ${status} <<<`);
-
   return grade;
 }
 
@@ -103,7 +57,7 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   //console.log("Hello, Commrade " + candidateName + "!");
   askQuestion();
-  console.log("gradeQuiz passes back: " + gradeQuiz(this.candidateAnswers));
+  gradeQuiz(this.candidateAnswers);
 }
 
 // Don't write any code below this line //
